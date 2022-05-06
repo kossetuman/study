99194720da7a4ee2abb2a29bd8c2a121c1192546
kossetuman <93471780+kossetuman@users.noreply.github.com>
2022-05-06 06:15:08 +0000

    [create]ログアウト機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user) # ログイン後に遷移するpathを設定
   end
+  
+  def after_sign_out_path_for(resource)
+     new_user_session_path 
+  end
+  
+  
 
   protected
 
